a02aa08b2783dee215eaa7d98afc97052141ce27
ach0210s ach <ach0210s ach>
2016-02-12 09:11:57 +0100

    ajout divers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,19 @@
         <title>ASCII - @yield('title')</title>
         <!-- Bootstrap minified css -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        
         <!-- FontAwesome minified css -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        
         <!-- Custom css -->
         <link rel="stylesheet" href="{{Request::root()}}/assets/css/style.css">
         
+        <!-- shortcut icon -->
         <link rel="shortcut icon" href="{{Request::root()}}/assets/image/favicon.PNG" />
+        
+        <!-- Chrome for Android -->
+        <meta name="theme-color" content="#010135">
+        <link rel="icon" sizes="192x192" href="{{Request::root()}}/assets/image/favicon.PNG">
         
         @yield('extra-head')
     </head>
